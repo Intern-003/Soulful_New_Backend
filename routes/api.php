fa517bc0680f1de493c5bff985da173ab7e3f2cd
Intern-003 <intern@spay.live>
2026-03-13 15:47:26 +0530

    dev 1

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\API\AuthController;
-
+use App\Http\Controllers\API\User\CategoryController;
+use App\Http\Controllers\API\User\ProductController;
+use App\Http\Controllers\API\Common\AuthController;
+use App\Http\Controllers\API\User\VendorStoreController;
 
 Route::post('auth/register', [AuthController::class, 'register']);
 Route::post('auth/login', [AuthController::class, 'login']);
@@ -25,3 +26,46 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::post('auth/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('auth/reset-password', [AuthController::class, 'resetPassword']);
+
+
+Route::prefix('categories')->group(function () {
+
+    Route::get('/', [CategoryController::class, 'index']);
+    Route::get('/{id}', [CategoryController::class, 'show']);
+    Route::get('/{id}/children', [CategoryController::class, 'children']);
+    Route::get('/{slug}/products', [CategoryController::class,'products']);
+});
+
+
+Route::prefix('products')->group(function () {
+
+    // Special routes FIRST
+    Route::get('/search', [ProductController::class, 'search']);
+    Route::get('/featured', [ProductController::class, 'featured']);
+    Route::get('/latest', [ProductController::class, 'latest']);
+    Route::get('/deals', [ProductController::class, 'deals']);
+    Route::get('/best-sellers', [ProductController::class, 'bestSellers']);
+
+    // Listing
+    Route::get('/', [ProductController::class, 'index']);
+
+    // Related
+    Route::get('/{id}/related', [ProductController::class, 'related']);
+
+
+    // Product detail LAST
+    Route::get('/{id}/images',[ProductController::class,'images']);
+    Route::get('/{id}/reviews',[ProductController::class,'reviews']);
+    Route::get('/{id}/rating',[ProductController::class,'rating']);
+    Route::get('/{slug}', [ProductController::class, 'show']);
+    
+});
+
+Route::prefix('vendors')->group(function(){
+
+Route::get('/',[VendorStoreController::class,'index']);
+Route::get('/{slug}',[VendorStoreController::class,'show']);
+Route::get('/{slug}/products',[VendorStoreController::class,'products']);
+Route::get('/{slug}/reviews',[VendorStoreController::class,'reviews']);
+
+});
