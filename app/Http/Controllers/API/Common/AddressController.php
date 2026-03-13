b82da0cc693fbad95b9d8996015d30f04655f474
intern-003 <intern@spay.live>
2026-03-13 15:38:12 +0530

    New

--- app/Http/Controllers/API/Common/AddressController.php
+++ app/Http/Controllers/API/Common/AddressController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers\API\Common;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Address;
 
 class AddressController extends Controller
 {
-    //
+    public function getAddresses(Request $request){
+        $addresses=Address::all();
+
+        return response()->json($addresses);
+    }
+
+    public function getAddressById(Request $request,int $id){
+        $address=Address::findOrFail($id);
+
+        return response()->json($address);
+    }
 }
